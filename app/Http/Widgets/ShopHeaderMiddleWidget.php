dc30befc466e54b9a4417e1d5874476d4561e75c
Alexandr Chernyaev <bliz48rus@gmail.com>
2018-07-01 20:00:44 +0000

    Apply fixes from StyleCI

--- app/Http/Widgets/ShopHeaderMiddleWidget.php
+++ app/Http/Widgets/ShopHeaderMiddleWidget.php
@@ -8,22 +8,21 @@ use Illuminate\Support\Facades\Cache;
 use Orchid\Platform\Core\Models\Menu;
 use Orchid\Platform\Widget\Widget;
 
-class ShopHeaderMiddleWidget extends Widget {
-
+class ShopHeaderMiddleWidget extends Widget
+{
     /**
      * @return mixed
      */
-     public function handler(){
-
-         $menu = Cache::remember('shop-header-middle'.App::getLocale(), Carbon::now()->addHour(), function () {
-             return Menu::where('lang', App::getLocale())
+    public function handler()
+    {
+        $menu = Cache::remember('shop-header-middle'.App::getLocale(), Carbon::now()->addHour(), function () {
+            return Menu::where('lang', App::getLocale())
                  ->where('type', 'shop-header-middle')
                  ->get();
-         });
+        });
 
-         return view('partials.header.shopHeaderMiddle',[
+        return view('partials.header.shopHeaderMiddle', [
             'menu' => $menu,
          ]);
-     }
-
+    }
 }
