3bebfe26fa92c054b9b13b1d2df8f28521d8a332
funtom48 <ivan40in48@gmail.com>
2018-07-02 12:20:47 +0300

    add social share

--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -116,6 +116,17 @@
                         </div>
                     </div>
 
+                    <script type="text/javascript">(function() {
+                      if (window.pluso)if (typeof window.pluso.start == "function") return;
+                      if (window.ifpluso==undefined) { window.ifpluso = 1;
+                        var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                        s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                        s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                        var h=d[g]('body')[0];
+                        h.appendChild(s);
+                      }})();</script>
+                    <div class="m-t-md pluso" data-background="transparent" data-options="medium,square,line,horizontal,nocounter,theme=06" data-services="vkontakte,odnoklassniki,facebook,google" data-url="https://liptur.orchid.software"></div>
+
                     <div class="m-t-md">
                         <div class="b-t">
                             <div class="wrapper">
