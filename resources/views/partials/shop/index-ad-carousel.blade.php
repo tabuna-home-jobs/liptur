fcb383ee188b8702cbdb6f4da68bc3a15631bb4f
PSalador <salador2005@yandex.ru>
2018-07-30 15:44:11 +0000

    No attachment

--- resources/views/partials/shop/index-ad-carousel.blade.php
+++ resources/views/partials/shop/index-ad-carousel.blade.php
@@ -1,10 +1,22 @@
+@php
+    $page = Orchid\Platform\Core\Models\Page::where('slug','carousel-links')->with('attachment')->first();
+    dd($page);
+@endphp
+
 <div class="bg-white padder-v">
     <div class="container no-padder">
+      <svg class="defs-only">
+        <filter id="monochrome">
+           <feColorMatrix in="SourceGraphic" 
+                          type="matrix" 
+                          values=".25 .10 .13 0 0 0 .01 0 0 0 0 0 .01 0 0 0 0 0 1 0 "></feColorMatrix>
+        </filter>
+      </svg>
       <div class="owl-carousel ad-carousel">
         @for($i=1; $i<6; $i++)
            <figure class="item">
               <a href="#">
-                <img class="owl-lazy img-hovers img-responsive" data-src="/img/carusel/b-{{$i}}.png" data-hover-src="/img/carusel/color/b-{{$i}}.png">
+                <img class="owl-lazy img-responsive" src="/img/carusel/color/b-{{$i}}.png">
               </a>
           </figure>
         @endfor
