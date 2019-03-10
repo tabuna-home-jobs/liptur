33849d052b7e80c2073716a2772fe693529bfdf4
funtom48 <ivan40in48@gmail.com>
2019-03-11 00:46:39 +0300

    add template

---
+++ resources/views/partials/widgets/menu/menu-shop-header-mobile.blade.php
@@ -0,0 +1,15 @@
+<div class="collapse navbar-collapse m-t-md b-b no-padder menu-shop-header {{((Request::is('shop')) ||(Request::is('shop/*')))?'':'menu-site-header'}}">
+    <ul class="nav navbar-nav">
+        <li class="hidden-xs">
+            <button class="btn btn-link" type="button" data-toggle="modal" data-target="#topmenu">
+                <i class="fa fa-bars"></i>
+            </button>
+        </li>
+        {!! $menuitem !!}
+        {{--
+        @include('partials.widgets.menu.menuitem',[
+            'menu'=>$menu
+        ])
+        --}}
+    </ul>
+</div>
