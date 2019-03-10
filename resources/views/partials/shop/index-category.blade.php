3ed7a172186360383fe25a024c2a85fc675d3d9e
funtom48 <ivan40in48@gmail.com>
2019-03-11 01:31:23 +0300

    add title into product catalog carousel

--- resources/views/partials/shop/index-category.blade.php
+++ resources/views/partials/shop/index-category.blade.php
@@ -1,14 +1,17 @@
-<div class="wrapper bg-white main-category padder-v">
-    <div class="container shop-categories-widget">
-        <div class="owl-carousel category-carousel">
-            @foreach($categories as $category)
-                <figure class="item">
-                    <a href="{{route('shop.products',$category->slug)}}">
-                        <img src="@if($category->term->getContent('smallPicture')!=""){{$category->term->getContent('smallPicture')}} @else /img/icons/slon.png @endif"/>
-                        <p>{{$category->term->getContent('name')}}</p>
-                    </a>
-                </figure>
-            @endforeach
-        </div>
+<div class="wrapper bg-white main-category">
+  <div class="block-header container pt-3 padder-v">
+    Каталог товаров
+  </div>
+  <div class="container shop-categories-widget">
+    <div class="owl-carousel category-carousel">
+      @foreach($categories as $category)
+        <figure class="item">
+          <a href="{{route('shop.products',$category->slug)}}">
+            <img src="@if($category->term->getContent('smallPicture')!=""){{$category->term->getContent('smallPicture')}} @else /img/icons/slon.png @endif"/>
+            <p>{{$category->term->getContent('name')}}</p>
+          </a>
+        </figure>
+      @endforeach
     </div>
+  </div>
 </div>
